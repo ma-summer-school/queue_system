e8e1b862c1543e5a142784a830aa4ed03f379e00
Eleni Hikiko <eleni@qurashee>
2014-07-18 07:57:58 +0300

    a very bad example of the PhpSerial class usage

---
+++ serial.php
@@ -0,0 +1,45 @@
+<html>
+
+<body>
+<?php
+include 'PhpSerial.php';
+
+$serial = new PhpSerial;
+$serial->deviceSet("/dev/ttyUSB0");
+$serial->confBaudRate(38400);
+$serial->confParity("none");
+$serial->confCharacterLength(8);
+$serial->confStopBits(1);
+$serial->confFlowControl("none");
+
+$serial->deviceOpen('r+');
+
+#$serial->sendMessage("h\n");
+#print "help:".$serial->readPort()."\n";
+
+print "<table border=1><tbody>\n";
+print "<tr>\n";
+print "<th>Επόμενος πελάτης</th>\n";
+print "<th>Σειρά (τελευταίο χαρτάκι)</th>\n";
+print "</tr>\n";
+
+print "<tr>\n";
+print "<td>";
+$serial->sendMessage("c\n");
+preg_match('/\d+/', $serial->readPort(), $read);
+if ($read)
+	print $read[0];
+print "</td>\n";
+print "<td>";
+$serial->sendMessage("t\n");
+preg_match('/\d+/', $serial->readPort(), $read);
+if ($read)
+	print $read[0];
+print "</td>\n";
+print "</tr>\n";
+print "</tbody></table>\n";
+
+$serial->deviceClose();
+?>
+</body>
+</html>
