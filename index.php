7e5572048c1de2edafbfb47d55a461aafe0cb7da
maellak <maellak@pioupiou>
2014-07-18 03:25:11 +0300

    modified:   index.php 	modified:   refresh.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
 $_SESSION["lastserved"]=isset($_SESSION["lastserved"]) ? $_SESSION["lastserved"] : 0 ;
-$_SESSION["lastticket"]=isset($_SESSION["lastticket"]) ? $_SESSION["lastticket"] : 0 ; 
+$_SESSION["lastticket"]=isset($_SESSION["lastticket"]) ? $_SESSION["lastticket"] : 0 ;
+/*if(isset($_SESSION['allowed']))
+print_r($_SESSION['allowed']);
+*/
 ?>
 
 <html>
@@ -10,29 +13,55 @@ $_SESSION["lastticket"]=isset($_SESSION["lastticket"]) ? $_SESSION["lastticket"]
   <link rel="stylesheet" type="text/css" href="style.css">
   <script src="jquery.js"></script>
   <script>
+  <!-- 
     if($(window).width() <= 400)
       document.location = "m.index.php";
+   -->   
   </script>
   <script type="text/javascript">//script refreshing every 5secs
+    <!--
     var auto_refresh = setInterval( function () { 
     $('#refresh').load('refresh.php').fadeIn("slow");
-    }, 500); // refresh every 10000 milliseconds 
+    }, 500); // refresh every 10000 milliseconds
+    --> 
   </script>
 </head
 <body>
 <?php
 if(isset($_POST['submit']))
 {
-   ++$_SESSION['lastticket'];
-}
-
-if(isset($_POST['amka']))
-{
-  $_POST['amka']=preg_replace("/[^0-9]/", "", $_POST['amka']);
-  if( strlen($_POST['amka'])!=11)
-  {
-    $_POST['amka']="";
-  } 
+  
+	if(isset($_POST['amka']))
+	{
+  	$_POST['amka']=preg_replace("/[^0-9]/", "", $_POST['amka']);
+  	   
+  		if( strlen($_POST['amka'])!=11  || !DateTime::createFromFormat('dd/mm/YY', substr($_POST['amka'],0,2).'/'.substr($_POST['amka'],2,2).'/'. substr($_POST['amka'],4,2))  )
+  		{	
+    	echo 'Λάθος ΑΜΚΑ, βεβαιωθείτε οτι ο ΑΜΚΑ που έχετε εισάγει είναι σωστός και προσπαθήστε ξανα';
+  		}
+  		else
+  		{
+  		    
+  			if(!isset($_SESSION['allowed']))
+  			{
+  			++$_SESSION['lastticket'];
+		  	$_SESSION['allowed'][$_POST['amka']]=$_SESSION['lastticket'];
+  			 
+  			 echo 'Ο ΑΜΚΑ σας έχει  καταχωρηθεί στο μητρώο. Παρακαλώ περάστε απο το ταμείο';
+  			}
+ 		 	else if(!isset($_SESSION['allowed'][$_POST['amka']]))
+  			{
+  			++$_SESSION['lastticket'];
+  			$_SESSION['allowed'][$_POST['amka']]=$_SESSION['lastticket'];
+  			 
+  			echo 'Ο ΑΜΚΑ σας έχει  καταχωρηθεί στο μητρώο. Παρακαλώ περάστε απο το ταμείο';
+			}
+			else
+			{
+			echo 'Ο ΑΜΚΑ σας είναι ήδη καταχωρημένος στο μητρώο!';
+			}
+  		} 
+	}
 }
 ?>
 
@@ -40,23 +69,23 @@ if(isset($_POST['amka']))
 	<div id="refresh" >
 		<div id="lastserved">
 
-<?php 
-    echo '<h2>Last Served:</h2><h1>'.$_SESSION["lastserved"].'</br></h1>';
-?>
+		<?php 
+    	echo '<h3>Αριθμός που εξυπηρετείται:</h2><h1>'.$_SESSION["lastserved"].'</br></h1>';
+		?>
 
 		</div>
 		<div id="lastticket" >
-
-<?php 
-    echo '<h2>Last ticket:</h2><h1>'.$_SESSION["lastticket"].'</h1>';
-?>
+	
+		<?php 
+    	echo '<h3>Τελευταίος αριθμός στην ουρά:</h2><h1>'.$_SESSION["lastticket"].'</h1>';
+		?>
 
 		</div>
 	</div>
 	<div id="form" >
     <form id="get_a_ticket" action="" method="POST">
-      <div id="Greek_Form" style="">
-        Επίθετο: <input type="text" name="Surname_Gr" >
+      
+       <!-- Επίθετο: <input type="text" name="Surname_Gr" >
         </br>
         Όνομα: <input type="text" name="Name_Gr" >
         </br>
@@ -68,7 +97,7 @@ if(isset($_POST['amka']))
         <input type="text" size="2" maxlength="2" name="DBirth"  > /
         <input type="text" size="2" maxlength="2" name="MBirth"  > /
         <input type="text" size="2" maxlength="2" name="YBirth"  >
-        </div>
+       -->
         ΑΜΚΑ: <input type="text" size="11" maxlength="11" name="amka" >
         </br>
         <input type="submit" value="submit" name="submit">
--- refresh.php
+++ refresh.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 $_SESSION["lastserved"]=isset($_SESSION["lastserved"]) ? $_SESSION["lastserved"] : 0 ;
 $_SESSION["lastticket"]=isset($_SESSION["lastticket"]) ? $_SESSION["lastticket"] : 0 ; 
@@ -8,13 +7,16 @@ $_SESSION["lastticket"]=isset($_SESSION["lastticket"]) ? $_SESSION["lastticket"]
 ?>
 
 		<div id="lastserved">
-		<?php 
-				echo '<h2>Last Served:</h2><h1>'.$_SESSION["lastserved"].'</br></h1>';
 
+		<?php 
+    	echo '<h3>Αριθμός που εξυπηρετείται:</h2><h1>'.$_SESSION["lastserved"].'</br></h1>';
 		?>
+
 		</div>
 		<div id="lastticket" >
+	
 		<?php 
-				echo '<h2>Last ticket:</h2><h1>'.$_SESSION["lastticket"].'</h1>';
+    	echo '<h3>Τελευταίος αριθμός στην ουρά:</h2><h1>'.$_SESSION["lastticket"].'</h1>';
 		?>
+
 		</div>
